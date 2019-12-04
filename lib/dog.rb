b2b668aa1e7267364c0a8b2564e7200a750b0212
Otto Cedeno <otto@ottostacos.com>
2019-12-04 02:34:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,7 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << self
+
   end
 
   def self.all
@@ -19,7 +19,7 @@ class Dog
   end
 
   def save
-
+    @@all << self
   end
 end
 
